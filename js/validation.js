8183d7457df287904896a61fb4b498f5b4c6d3c1
dimagri <demon-2008@inbox.ru>
2015-04-02 00:25:05 +0300

    rewrote validation

--- js/validation.js
+++ js/validation.js
@@ -10,23 +10,21 @@ $(document).ready(function(){
 		$(input).addClass("required");
 	}
 	
-	function errorInterval (input,from,to,submitbtn) {
-		//if (inputLength(input) > 0){
-			if (to == 0){
-				if (inputLength(input) < from){
-					noSubmit(submitbtn,input);
-				}
-			} else {
-				if ((inputLength(input) < from) || (inputLength(input) > to)){
-					noSubmit(submitbtn,input);
+	function checkInput (input,from,to,submitbtn,isRequired) {
+		if (inputLength(input) == 0 && isRequired){
+			noSubmit(submitbtn,input);
+		} else {
+			if (inputLength(input) > 0){
+				if (to == 0){
+					if (inputLength(input) < from){
+						noSubmit(submitbtn,input);
+					}
+				} else {
+					if ((inputLength(input) < from) || (inputLength(input) > to)){
+						noSubmit(submitbtn,input);
+					}
 				}
 			}
-		//}
-	}
-	
-	function disableIfEmpty (submitbtn,input) {
-		if (inputLength(input) == 0) {
-			noSubmit(submitbtn,input);
 		}
 	}
 	
@@ -37,7 +35,7 @@ $(document).ready(function(){
 	
 	
 	$(".add-comment input[type='submit']").click(function(){
-		errorInterval(".add-comment textarea",3,150,$(this))
+		checkInput(".add-comment textarea",3,150,$(this),true)
 		if ($(this).hasClass("disabled")) {
 			return false;
 		}
@@ -45,37 +43,33 @@ $(document).ready(function(){
 	
 	
 	$(".add_event-page input[type='submit']").click(function(){
-		disableIfEmpty($(this),"#event_name");
-		disableIfEmpty($(this),"#event_description");
-		disableIfEmpty($(this),"#event_phone");
-		errorInterval("#event_name",5,0,$(this));
-		errorInterval(".add_event-page textarea",20,0,$(this));
+		checkInput(".add_event-page #event_name",5,0,$(this),true);
+		checkInput(".add_event-page #event_description",20,0,$(this),true);
+		checkInput(".add_event-page #event_phone",0,0,$(this),true);
 		if ($(this).hasClass("disabled")) {
 			return false;
 		}
 	});
 	
 	$(".send-message input[type='submit']").click(function(){
-		disableIfEmpty($(this),".send-message #message_body");
-		errorInterval(".send-message #message_body",0,1000,$(this));
-		disableIfEmpty($(this),".send-message #message_subject");
-		errorInterval(".send-message #message_subject",4,140,$(this));
+		checkInput(".send-message #message_body",0,1000,$(this),true);
+		checkInput(".send-message #message_subject",4,140,$(this),true);
 		if ($(this).hasClass("disabled")) {
 			return false;
 		}
 	});
 	
 	$(".edit-usr-page input[type='submit']").click(function(){
-		errorInterval(".edit-usr-page #user_about",0,100,$(this));
-		errorInterval(".edit-usr-page #user_phone",6,10,$(this));
+		checkInput(".edit-usr-page #user_about",0,100,$(this),false);
+		checkInput(".edit-usr-page #user_phone",6,10,$(this),false);
 		if ($(this).hasClass("disabled")) {
 			return false;
 		}
 	});
 	
 	$(".enter-popup input[type='submit']").click(function(){
-		disableIfEmpty($(this),".enter-popup #email");
-		disableIfEmpty($(this),".enter-popup #password");
+		checkInput(".enter-popup #email",0,0,$(this),true);
+		checkInput(".enter-popup #password",0,0,$(this),true);
 		if ($(this).hasClass("disabled")) {
 			return false;
 		}
@@ -86,10 +80,9 @@ $(document).ready(function(){
 		if (!(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test($(".register-popup #user_email").val()))){
 			noSubmit($(this),"#user_email");
 		}
-		disableIfEmpty($(this),".register-popup #user_email");
-		disableIfEmpty($(this),".register-popup #user_name");
-		disableIfEmpty($(this),".register-popup #user_password");
-		disableIfEmpty($(this),".register-popup #user_password_confirmation");
+		checkInput(".register-popup #user_name",0,0,$(this),true);
+		checkInput(".register-popup #user_password",0,0,$(this),true);
+		checkInput(".register-popup #user_password_confirmation",0,0,$(this),true);
 		if ($(this).hasClass("disabled")) {
 			return false;
 		}
